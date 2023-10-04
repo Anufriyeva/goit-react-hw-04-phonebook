ab8fae7c8e4b6f5a2be2c552688f2a1e2d75cd5e
Anufriyeva <126349829+Anufriyeva@users.noreply.github.com>
2023-10-04 11:18:36 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import {AppContainer} from './App.styles';
+import { nanoid } from 'nanoid';
+
+const saveToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getFromLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  // state = {
+  //   contacts: [
+  //     {id: 'id-1', name: 'John Doe', number: '459-12-56'},
+  //     {id: 'id-2', name: 'Tyler Durden', number: '443-89-12'},
+  //     {id: 'id-3', name: 'Vincent Vega', number: '645-17-79'},
+  //     {id: 'id-4', name: 'Irene Adler', number: '227-91-26'},
+  //   ],
+  //   filter: '',
+  //   name: '',
+  //   number: ''
+  // }
+
+  componentDidMount() {
+    const savedContacts = getFromLocalStorage('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      saveToLocalStorage('contacts', this.state.contacts);
+    }
+  }
+
+  handleFilterChange = (filterValue) => {
+    this.setState({ filter: filterValue });
+  };
+
+  createContact = newContactData => {
+    const isExist = this.state.contacts.find(
+      contact => contact.name === newContactData.name
+    );
+
+    if (isExist) {
+      return alert(`${newContactData.name} is already in contacts`);
+    }
+    const newContact = {
+      ...newContactData,
+      id: nanoid(),
+    };
+
+    this.setState(prev => ({
+      contacts: [newContact, ...prev.contacts],
+    }));
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+
+    return (
+      <AppContainer>
+        <h1>Phonebook</h1>
+        <ContactForm
+          createContact={this.createContact}
+        />
+        <h2>Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
+
